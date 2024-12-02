fb7bed880180c3ea3f8bee4101615cd609a73bc1
temperance <3785923+tmprnc@users.noreply.github.com>
2024-12-02 22:09:48 +0100

    failed attempt at collapsibles lol

--- scripts/arc-ui.js
+++ scripts/arc-ui.js
@@ -561,3 +561,19 @@ function showabout() {
 function hideabout() {
   setvisibility(document.getElementById('about'), "i")
 }
+
+/* Not working
+var coll = document.getElementsByClassName("clps-btn");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("clps-active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+} */
